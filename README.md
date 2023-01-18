5f786edd12afbca8293d2e3f8ddcfef3d9521266
David <david@davidcraft.de>
2023-01-18 17:27:48 +0100

    Initial commit
    widget.js:
    Signed-off-by: David <david@davidcraft.de>
    Workflows:
    Signed-off-by: Zoey <zoey@z0ey.de>

---
+++ README.md
@@ -0,0 +1,15 @@
+# Wunderground PWA Lockscreen Widget
+A simple Scriptable lockscreen widget that view data from a Wunderground PWA on the iOS Lockscreen
+
+<img src="https://github.com/ZoeyVid/wunderground-pwa-scriptable-lockscreen-widget/raw/main/example.jpeg">
+
+## Config 
+Please insert your station id (You can find it here: https://www.wunderground.com/member/devices) and your Wunderground api key (You can find it here: https://www.wunderground.com/member/api-keys) on the first two variabels 
+
+```js
+// Insert here your station id (https://www.wunderground.com/member/devices)
+let stationID = ""
+
+// Insert your Wunderground api key here (https://www.wunderground.com/member/api-keys)
+let apiKey = ""
+```
---
+++ widget.js
@@ -0,0 +1,19 @@
+// Version: 0.0.0
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: blue; icon-glyph: umbrella;
+// Insert here your station id (https://www.wunderground.com/member/devices)
+let stationID = ""
+
+// Insert your Wunderground api key here (https://www.wunderground.com/member/api-keys)
+let apiKey = ""
+
+let loadedJSON = await new Request("https://api.weather.com/v2/pws/observations/current?stationId=" + stationID + "&format=json&units=m&apiKey=" + apiKey).loadJSON()
+
+let widget = new ListWidget()
+let widgetHeadline = widget.addText(String(loadedJSON.observations[0].neighborhood))
+widgetHeadline.font = Font.headline()
+widget.addText(String(loadedJSON.observations[0].metric.temp) + "℃")
+widget.addText(String(loadedJSON.observations[0].metric.precipTotal) + "L")
+Script.setWidget(widget)
+widget.presentSmall()
